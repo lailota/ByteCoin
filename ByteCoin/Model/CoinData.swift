da403c3b3cccffb0f1ccbd876d4280c6d3c98303
Laila GH <laila.guzzon@gmail.com>
2021-05-06 12:13:22 -0300

    Create new CoinData

--- ByteCoin/Model/CoinData.swift
+++ ByteCoin/Model/CoinData.swift
@@ -2,8 +2,12 @@
 //  CoinData.swift
 //  ByteCoin
 //
-//  Created by Laila Guzzon Hussein Lailota on 29/01/21.
+//  Created by Laila Guzzon Hussein Lailota on 06/05/21.
 //  Copyright © 2021 The App Brewery. All rights reserved.
 //
 
 import Foundation
+
+struct CoinData: Decodable {
+    let rate: Double
+}
