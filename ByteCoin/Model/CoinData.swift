7056dbff1b238716804c088c09215a66f85af5eb
Laila GH <laila.guzzon@gmail.com>
2021-05-06 10:55:48 -0300

    Add .gitignore file

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,9 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Laila Guzzon Hussein Lailota on 29/01/21.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
